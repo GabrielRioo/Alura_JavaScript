4528e6201873f3bd15769572081e991356d055e3
GabrielRioo <gabrielpaivario@gmail.com>
2021-08-06 21:02:59 -0300

    formulario para adicionar mais um item a tabela

--- ProgramandoNaWeb/introducao-javascript/js/index.js
+++ ProgramandoNaWeb/introducao-javascript/js/index.js
@@ -2,14 +2,14 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Batata";
 
 titulo.addEventListener("click", () => {
-    console.log('fui clicado')
+  console.log("fui clicado");
 });
 
 var pacientes = document.querySelectorAll(".paciente");
 // console.log(pacientes);
 
 for (let i = 0; i < pacientes.length; i++) {
-    var paciente = pacientes[i];
+  var paciente = pacientes[i];
 
   var peso = paciente.querySelector(".info-peso").textContent;
   var altura = paciente.querySelector(".info-altura").textContent;
@@ -42,10 +42,38 @@ for (let i = 0; i < pacientes.length; i++) {
 }
 
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener('click', (event) => {
-    event.preventDefault();
-    console.log('fui clicado');
-})
+botaoAdicionar.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  var form = document.querySelector("#form-adiciona");
+  var nome = form.nome.value;
+  var peso = form.peso.value;
+  var altura = form.altura.value;
+  var gordura = form.gordura.value;
+
+  var pacienteTr = document.createElement("tr");
+  var nomeTd = document.createElement("td");
+  var pesoTd = document.createElement("td");
+  var alturaTd = document.createElement("td");
+  var gorduraTd = document.createElement("td");
+  var imcTd = document.createElement("td");
+
+  nomeTd.textContent = nome;
+  pesoTd.textContent = peso;
+  alturaTd.textContent = altura;
+  gorduraTd.textContent = gordura;
+
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+
+  var tabela = document.querySelector("#tabela-pacientes");
+  tabela.appendChild(pacienteTr);
+
+  console.log(pacienteTr);
+
+});
 
 // console.log(tdImc);
 // console.log(paciente);
