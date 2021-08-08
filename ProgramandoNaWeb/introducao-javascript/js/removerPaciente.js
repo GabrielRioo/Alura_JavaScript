1c081c6ee94e0f2fd693e7d3b9d3494d93fa18fc
GabrielRioo <gabrielpaivario@gmail.com>
2021-08-07 22:16:44 -0300

    adicionando transição de opacidade ao deletar um item

--- ProgramandoNaWeb/introducao-javascript/js/removerPaciente.js
+++ ProgramandoNaWeb/introducao-javascript/js/removerPaciente.js
@@ -11,11 +11,21 @@
 
 // ------------------------------------
 var pacientes = document.querySelectorAll(".paciente");
+var tabela = document.querySelector("table");
 
-pacientes.forEach((paciente) => {
-    paciente.addEventListener('dblclick', () => {
-        this.remove();
-    })
+tabela.addEventListener('dblclick', (event) => {
+    console.log("aqui")
+    
+     event.target.parentNode.classList.add('fade-out');
+     setTimeout(() => {
+         event.target.parentNode.remove();
+     }, 500);
 })
 
-console.log(pacientes);
+// pacientes.forEach((paciente) => {
+//     paciente.addEventListener('dblclick', () => {
+//         this.remove();
+//     })
+// })
+
+// console.log(pacientes);
