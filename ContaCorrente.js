292da256438ae9326c4a65031fb3613c0993ecb4
GabrielRioo <gabrielpaivario@gmail.com>
2021-08-14 20:58:39 -0300

    sobreescrevendo metodos da classe mae

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -25,4 +25,15 @@ export class ContaCorrente extends Conta {
     get saldo() {
         return this._saldo;
     }
+
+    sacar(valor) {
+        let taxa = 1.1;
+        const valorSacado = taxa * valor;
+
+        if (this._saldo >= valorSacado) {
+            this._saldo -= valorSacado;
+
+            return valorSacado;
+        }
+    }
 }
