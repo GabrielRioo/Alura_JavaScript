f92f2f4539da3abe982a47b8a6948fd0e8b2c8b6
GabrielRioo <gabrielpaivario@gmail.com>
2021-08-15 00:29:56 -0300

    polimorfismo com o cadastro de senha

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
-import { Cliente } from './Cliente.js'
-import { ContaCorrente } from './Conta/ContaCorrente.js'
-import { ContaPoupanca } from './Conta/ContaPoupanca.js';
-import { ContaSalario } from './Conta/ContaSalario.js';
+import { Cliente } from "./Cliente.js";
+import { Gerente } from "./Funcionario/Gerente.js";
+import { Diretor } from "./Funcionario/Diretor.js";
+import { Funcionario } from "./Funcionario/Funcionario.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente('Gabriel', 11122233344);
-const contaCorrenteGabriel = new ContaCorrente(cliente1, 1001);
+const diretor = new Diretor("Gabriel", 10000, 12345678900);
+diretor.cadastrarSenha("123456789");
 
-contaCorrenteGabriel.depositar(300);
-contaCorrenteGabriel.sacar(100);
+const gerente = new Gerente("Camila", 5000, 98765432100);
+gerente.cadastrarSenha("123");
 
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+const diretorLogado = SistemaAutenticacao.login(diretor, "123456789");
+const gerenteLogado = SistemaAutenticacao.login(gerente, "123");
 
-const contaSalario = new ContaSalario(cliente1);
-contaSalario.depositar(100);
-contaSalario.sacar(10);
+console.log(`diretor: ${diretorLogado}`);
+console.log(`gerente: ${gerenteLogado}`);
+// ------------------------------------------------
 
-console.log(contaSalario);
-console.log(contaPoupanca);
-console.log(contaCorrenteGabriel);
+// import { Cliente } from './Cliente.js'
+// import { ContaCorrente } from './Conta/ContaCorrente.js'
+// import { ContaPoupanca } from './Conta/ContaPoupanca.js';
+// import { ContaSalario } from './Conta/ContaSalario.js';
+
+// const cliente1 = new Cliente('Gabriel', 11122233344);
+// const contaCorrenteGabriel = new ContaCorrente(cliente1, 1001);
+
+// contaCorrenteGabriel.depositar(300);
+// contaCorrenteGabriel.sacar(100);
+
+// const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+
+// const contaSalario = new ContaSalario(cliente1);
+// contaSalario.depositar(100);
+// contaSalario.sacar(10);
+
+// console.log(contaSalario);
+// console.log(contaPoupanca);
+// console.log(contaCorrenteGabriel);
