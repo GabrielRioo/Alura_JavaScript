f8e953da58ee7921841ddb1c7c4f676144b30a15
GabrielRioo <gabrielpaivario@gmail.com>
2022-07-12 22:18:42 -0300

    Adicionando som e onkey press do teclado

--- Javascript_FrontEnd/aluramidi/script/main.js
+++ Javascript_FrontEnd/aluramidi/script/main.js
@@ -1,6 +1,34 @@
-function TocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function TocaSom(audioID) {
+    console.log(audioID);
+    const elemento = document.querySelector(audioID);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('b')
+    }
+
+    
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
 // document.querySelector('.tecla_pom').onclick = TocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas.forEach((item) => {
+    var instrumento = item.classList[1];
+
+    item.onclick = () => {
+        TocaSom(`#som_${instrumento}`);
+    };
+
+    item.onkeydown = (event) => {
+        console.log(event.code)
+        if(event.code === 'Enter' || event.code === 'Space') 
+            item.classList.add('ativa')
+    }
+
+    item.onkeyup = (event) => {
+        if(event.code === 'Enter' || event.code === 'Space') 
+            item.classList.remove('ativa')
+    }
+})
